0c59f25b7b3ae19c48d6b06762709f4d4a44a076
kaminZero2 <kaminkoim@gmail.com>
2026-04-24 15:44:55 +0700

    Add files via upload

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Kamin"
+last_name = "Koim"
+whole_name = (first_name + "  " + last_name)
+print(whole_name)
---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+name = input("Hey, what's your name?")
+last_name = input("and your last name?")
+print("Well,pleased to meet you",{name +" "+ last_name})
